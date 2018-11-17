0beac70a517a47ac6823de698f5b8c326749ce1e
Adelino Afonso <adelino-afonso@hotmail.com>
2018-11-17 12:57:28 -0300

    tests

--- src/sd.cpp
+++ src/sd.cpp
@@ -31,61 +31,59 @@ void sd::injeta_pacote() {
 			}
 		}
 	}
-	cout << noc42->network[0][1]->mux_local->saida<<" " << noc42->network[1][0]->mux_local->saida/*<< " "<<noc42->network[2][1]->mux_local->saida */<< endl;
+	cout << noc42->network[0][1]->mux_local->saida<<" " << noc42->network[1][0]->mux_local->saida << " "<<noc42->network[2][1]->mux_local->saida<< " "<<noc42->network[0][0]->mux_local->saida << endl;
 	//  TODO: Fazer o sc_stop após o ultimo flit chegar no ultimo core
 }
 
 void sd::solicita_rota() {
 
   	
+	if (!solicitacoes_de_rota.empty())
+	{
+		switch(solicitacoes_de_rota.front()) {
+			case 0:
+				deque_pacotes[0].front().possui_rota = true;	
+				set_seletor[0][0][LESTE].write(tabela_mux[LESTE][LOCAL]);
+				set_enables[0][0][LESTE].write(1);
+				set_enables[0][1][LOCAL].write(1);
+				set_seletor[0][1][LOCAL].write(tabela_mux[LOCAL][OESTE]);
+				solicitacoes_de_rota.pop();
+			break;
+			case 1:
+				deque_pacotes[1].front().possui_rota = true;
+				set_seletor[0][1][OESTE].write(tabela_mux[OESTE][LOCAL]);
+				set_enables[0][1][OESTE].write(1);
+				set_enables[0][0][SUL].write(1);
+				set_seletor[0][0][SUL].write(tabela_mux[SUL][LESTE]);
+				set_enables[1][0][LOCAL].write(1);
+				set_seletor[1][0][LOCAL].write(tabela_mux[LOCAL][NORTE]);
+				solicitacoes_de_rota.pop();
+			break;
+			case 2:
+				deque_pacotes[2].front().possui_rota = true;
+				set_seletor[2][0][LESTE].write(tabela_mux[LESTE][LOCAL]);
+				set_enables[2][0][LESTE].write(1);
+				set_enables[2][1][LOCAL].write(1);
+				set_seletor[2][1][LOCAL].write(tabela_mux[LOCAL][OESTE]);
+				solicitacoes_de_rota.pop();
+			break;
+			case 3:
+				deque_pacotes[3].front().possui_rota = true;
+				set_seletor[2][2][OESTE].write(tabela_mux[OESTE][LOCAL]);
+				set_enables[2][2][OESTE].write(1);
+				set_enables[2][1][OESTE].write(1);
+				set_seletor[2][1][OESTE].write(tabela_mux[OESTE][LESTE]);
+				set_enables[2][0][NORTE].write(1);
+				set_seletor[2][0][NORTE].write(tabela_mux[NORTE][LESTE]);		
+				set_enables[1][0][NORTE].write(1);
+				set_seletor[1][0][NORTE].write(tabela_mux[NORTE][SUL]);	
+				set_enables[0][0][LOCAL].write(1);
+				set_seletor[0][0][LOCAL].write(tabela_mux[LOCAL][SUL]);									
+				solicitacoes_de_rota.pop();
+			break;
+		}
+	}
 
 
-
-	deque_pacotes[0].front().possui_rota = true;
-	deque_pacotes[1].front().possui_rota = true;
-	// deque_pacotes[2].front().possui_rota = true;
-
-	set_seletor[0][0][LESTE].write(tabela_mux[LESTE][LOCAL]);
-	set_enables[0][0][LESTE].write(1);
-	set_enables[0][1][LOCAL].write(1);
-	set_seletor[0][1][LOCAL].write(tabela_mux[LOCAL][OESTE]);
-
-	set_seletor[0][1][OESTE].write(tabela_mux[OESTE][LOCAL]);
-	set_enables[0][1][OESTE].write(1);
-
-	set_enables[0][0][SUL].write(1);
-	set_seletor[0][0][SUL].write(tabela_mux[SUL][LESTE]);
-
-	set_enables[1][0][LOCAL].write(1);
-	set_seletor[1][0][LOCAL].write(tabela_mux[LOCAL][NORTE]);
-
-
-	set_seletor[2][0][LESTE].write(tabela_mux[LESTE][LOCAL]);
-	set_enables[2][0][LESTE].write(1);
-	set_enables[2][1][LOCAL].write(1);
-	set_seletor[2][1][LOCAL].write(tabela_mux[LOCAL][OESTE]);
 
 }
-
-
-
-
-
-
-
-	// set_seletor[0][0][LESTE].write(tabela_mux[LESTE][LOCAL]);
-	// set_enables[0][0][LESTE].write(1);
-	// set_enables[0][1][LOCAL].write(1);
-	// set_seletor[0][1][LOCAL].write(tabela_mux[LOCAL][OESTE]);
-
-	// set_seletor[0][1][OESTE].write(tabela_mux[OESTE][LOCAL]);
-	// set_enables[0][1][OESTE].write(1);
-
-	// set_enables[0][0][LESTE].write(1);
-	// set_seletor[0][0][LESTE].write(tabela_mux[SUL][LESTE]);
-
-	// set_enables[0][0][SUL].write(1);
-	// set_seletor[0][0][SUL].write(tabela_mux[SUL][LESTE]);
-
-	// set_enables[1][0][LOCAL].write(1);
-	// set_seletor[1][0][LOCAL].write(tabela_mux[LOCAL][NORTE]);
